747452ee4274f3e2e79b35d3e7f05ae92d100d13
Attie Grande <attie@attie.co.uk>
2013-10-27 13:45:49 +0000

    added _lga_get_array() for use at file-scope

--- lga.h
+++ lga.h
@@ -17,42 +17,74 @@
         INSERT BEFORE .bss;
 */
 
+/* this macro associates a variable with an ELF section and is not intended to be used by the user */
+#define __lga_attrib(section_name) \
+	__attribute__((unused, section(".lga_"#section_name)))
+
 /* lga_el(type, array_name, element_name)
+   lga_terminator(type, array_name, element_name)
 
    is used to declare a linker-generated array element
    this macro will declare the variable for you
    you can retrieve the specific element using lga_get_element() later
 
+   lga_terminator() is designed for placing an element at the end of an array
+
    example usage:
      lga_el(type, numbers, special_number) = 12;
 */
 #define lga_el(type, array_name, element_name) \
-	type _lga_##array_name##_##element_name __attribute__((unused, section(".lga_"#array_name"_2_"#element_name)))
+	type _lga_##array_name##_##element_name __lga_attrib(#array_name"_2_"#element_name)
+#define lga_terminator(type, array_name, element_name) \
+	type _lga_##array_name##_##element_name __lga_attrib(#array_name"_3_"#element_name)
 
 /* _lga_el(array_name)
+   _lga_terminator(array_name)
 
    is used to TAG a variable as a member of a linker-generated array
    this marco will not declare the variable for you
    you can't retrieve the specific element later
 
+   _lga_terminator() is designed for placing an element at the end of an array
+
    example usage:
      int myInt _lga_el(numbers) = 10;
 */
 #define _lga_el(array_name) \
-	__attribute__((unused, section(".lga_"#array_name"_2")))
+	__lga_attrib(#array_name"_2")
+#define _lga_terminator(array_name) \
+	__lga_attrib(#array_name"_3")
 
 /* lga_get_array(array_name)
 
    is used to get the first element of a linker-generated array
+   this macro is for use within a function, as it is not valid at file-scope
+   for file-scope access, see _lga_get_array()
 
    example usage:
      int *x = lga_get_array(numbers);
 */
 #define lga_get_array(array_name) \
 	({ \
-		static char a[0] __attribute__((unused, section(".lga_"#array_name"_1"))); \
+		static char a[0] __lga_attrib(#array_name"_1"); \
 		(void*)&a; \
 	})
+
+/* _lga_get_array(array_name)
+
+   is used to declare an access variable for the array
+   this macro should be used at flie-scope, as lga_get_array() is invalid there
+
+   example usage:
+     _lga_get_array(int, numbers, numberArray);
+
+     void func(void) {
+       printf("number[0]: %d\n", numberArray[0]);
+     }
+*/
+#define _lga_get_array(type, array_name, variable_name) \
+	static char __lga_##variable_name[0] __lga_attrib(#array_name"_1"); \
+	type *variable_name = &__lga_##variable_name;
 
 /* lga_get_element(type, array_name, element_name)
 
@@ -81,7 +113,7 @@
 */
 #define lga_get_array_end(array_name) \
 	({ \
-		static char a[0] __attribute__((unused, section(".lga_"#array_name"_3"))); \
+		static char a[0] __lga_attrib(#array_name"_4"); \
 		(void*)&a; \
 	})
 
